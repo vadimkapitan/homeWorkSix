d4424ba97983099694e55c11ebe1a16a7fa7533b
vadimKapitan <xxxcap13@gmail.com>
2023-11-22 22:22:22 +0300

    Start

---
+++ src/main/java/pro/sky/homeWorkSix/HomeWorkSixApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWorkSix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSixApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pro/sky/homeWorkSix/service/EmployeeService.java
@@ -0,0 +1,5 @@
+package pro.sky.homeWorkSix.service;
+
+public class EmployeeService {
+
+}
